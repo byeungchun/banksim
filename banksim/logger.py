5ec6c7973cd0b255c5bf26ea19a3bb42184bbe41
Kwon, Byeungchun <ktwist@naver.com>
2019-02-26 18:58:27 +0100

    Create logger.py

---
+++ banksim/logger.py
@@ -0,0 +1,19 @@
+import logging, os, sys
+from logging.handlers import RotatingFileHandler
+
+cur_dir = os.path.abspath(os.curdir)
+sys.path.append(cur_dir)
+PROJECT_HOME = cur_dir
+
+def get_logger(name):
+    """
+    :param name:
+    :return:
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    rotate_handler = RotatingFileHandler(PROJECT_HOME + "/logs/" + name + ".log", 'a', 1024*1024*5, 5)
+    formatter = logging.Formatter('[%(levelname)s]-%(asctime)s-%(filename)s:%(lineno)s:%(message)s', datefmt="%m%d %H%M%S")
+    rotate_handler.setFormatter(formatter)
+    logger.addHandler(rotate_handler)
+    return logger
