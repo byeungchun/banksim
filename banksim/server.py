2b0a6c0d45152cf9313afcf23e9130cf0cac66b6
Kwon, Byeungchun <ktwist@naver.com>
2019-02-28 18:43:37 +0100

    Update server.py

--- banksim/server.py
+++ banksim/server.py
@@ -27,11 +27,13 @@ def mesa_abba_network_portrayal(G):
 canvas_network = NetworkModule(mesa_abba_network_portrayal, 500, 600, library='sigma')
 chart_element = ChartModule([{"Label":"BankAsset","Color":"#AA0000"}])
 
-model_params = {"initial_saver": UserSettableParameter("slider", "# of Saver", 10000, 10000, 20000, 100),
+model_params = {"max_steps": UserSettableParameter("slider", "Max steps", 20, 10, 200, 1),
+                "initial_saver": UserSettableParameter("slider", "# of Saver", 10000, 10000, 20000, 100),
                 "initial_bank": UserSettableParameter("slider", "# of Bank", 10, 10, 20, 1),
                 "initial_loan": UserSettableParameter("slider", "# of Loan", 20000, 10000, 30000,100),
                 "initial_equity": UserSettableParameter("slider", "Initial Equity of Bank", 100, 100, 200,1),
                 "car": UserSettableParameter("slider", "Min capital adequacy ratio", 0.08, 0.01, 0.10, 0.01)}
 
 server = ModularServer(BankSim, [canvas_network, chart_element], "ABBA - Banking system simulation", model_params)
+
 server.port = 8521
