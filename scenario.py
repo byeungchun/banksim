6458b1cda9f9f997f7c5f0428507cfe21cb9bea1
Kwon, Byeungchun <ktwist@naver.com>
2019-03-11 09:19:59 +0100

    Update scenario.py
    Parallel  function added

--- scenario.py
+++ scenario.py
@@ -1,25 +1,60 @@
 # coding: utf-8
 
+import itertools
+import concurrent.futures
+
+from banksim.logger import get_logger
 from banksim.model import BankSim
+from banksim.util.write_sqlitedb import init_database
 from banksim.agent.bank import Bank
 
-# To replicate ABBA model
-model_params = {"write_db": True,
-                "max_steps": 240,
-                "initial_saver": 10000,
-                "initial_bank": 10,
-                "initial_loan": 20000,
-                "initial_equity": 100,
-                "car": 0.08,
-                "rfree": 0.01,
-                "min_reserves_ratio": 0.03
-                }
+# To replicate the simulation result in this paper (ABBA: An Agent-Based Model of the Banking System - IMF)
+#
+#
 
-model = BankSim(**model_params)
+logger = get_logger("scenario")
+
+def exec_banksim_model(model_params):
+    model = BankSim(**model_params)
+    model.run_model(step_count=240)
+    return True
+
+def main():
+    model_params = {"init_db": False,
+                    "write_db": True,
+                    "max_steps": 240,
+                    "initial_saver": 10000,
+                    "initial_bank": 10,
+                    "initial_loan": 20000,
+                    "initial_equity": 100,
+                    "rfree": 0.01
+                    }
+
+    lst_capital_req = [0.04, 0.08, 0.12, 0.16]
+    lst_reserve_ratio = [0.03, 0.045, 0.06]
+    combination_car_res = list(itertools.product(lst_capital_req, lst_reserve_ratio))
+
+    init_database()
+
+    lst_model_params = list()
+    for x in combination_car_res:
+        model_params["car"] = x[0]
+        model_params["min_reserves_ratio"] = x[1]
+        lst_model_params.append(model_params.copy())
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        model_finish_cnt = 0
+        for res in executor.map(exec_banksim_model, lst_model_params):
+            if res:
+                model_finish_cnt = model_finish_cnt + 1
+                if model_finish_cnt % 10 == 0:
+                    logger.info('Number of completed scenario: %3d', model_finish_cnt)
 
 # Bank status
-for x in [x for x in model.schedule.agents if isinstance(x, Bank)]:
-    print("Bank{0:1d} - Reserve: {1:5.0f}, Equity: {2:5.0f}, Deposit: {3:5.0f}, Reserve_ratio: {4:5.3f}".
-          format(x.pos, x.bank_reserves, x.equity, x.bank_deposits, x.reserves_ratio))
+# for x in [x for x in model.schedule.agents if isinstance(x, Bank)]:
+#     print("Bank{0:1d} - Reserve: {1:5.0f}, Equity: {2:5.0f}, Deposit: {3:5.0f}, Reserve_ratio: {4:5.3f}".
+#           format(x.pos, x.bank_reserves, x.equity, x.bank_deposits, x.reserves_ratio))
 
-model.run_model(step_count=240)
+
+if __name__ == "__main__":
+    main()
